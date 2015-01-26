13e96cb09846463f64c7d75add851e490e7c9aaf
jamalping <silvercreek@Ryans-MBA.local>
2015-01-26 12:03:05 +0800

    Initial Commit

---
+++ waterAreaDeme/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  waterAreaDeme
+//
+//  Created by jamalping on 15-1-26.
+//  Copyright (c) 2015年 jamalping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ waterAreaDeme/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  waterAreaDeme
+//
+//  Created by jamalping on 15-1-26.
+//  Copyright (c) 2015年 jamalping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
